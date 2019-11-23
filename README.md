35aef428b0ed34af388f712b6947fc78a53bc19a
Maciej Malecki <maciej@smefju.pl>
2019-11-23 12:46:24 +0100

    Initialize Umbrella project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Brain
+
+## Requirements
+
+1. Elixir 1.9.x
+1. Erlang 22.0.x
---
+++ config/config.exs
@@ -0,0 +1,9 @@
+import Config
+
+# Sample configuration:
+#
+#     config :logger, :console,
+#       level: :info,
+#       format: "$date $time [$level] $metadata$message\n",
+#       metadata: [:user_id]
+#
