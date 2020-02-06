2663095d624c342fc6b859de289a5d90ed88fcf0
Maciej Malecki <maciej@smefju.pl>
2020-02-06 18:38:27 +0100

    Show data from the last 12 hours on dashboard.

--- apps/interface/lib/interface.ex
+++ apps/interface/lib/interface.ex
@@ -20,7 +20,7 @@ defmodule Interface do
         ]
   @type point :: %{x: DateTime.t(), y: Measurement.value() | RelayState.value()}
 
-  @within 3200
+  @within 60 * 60 * 12
 
   defdelegate relay_id_to_sensor_id(id), to: Collector
   defdelegate subscribe_to_storage, to: Collector, as: :subscribe
